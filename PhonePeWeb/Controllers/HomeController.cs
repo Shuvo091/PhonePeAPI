560c212d7702777fde357a71de5df25ec158e50d
Mehedi Hasan Shuvo <mehedi.shuvo@enosisbd.com>
2024-01-08 01:00:27 +0600

    Add project files.

---
+++ PhonePeWeb/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using ArchitectureLibraryCreditCardBusinessLayer;
+using ArchitectureLibraryCreditCardModels;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhonePeWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult PhonePe()
+        {
+            //CreditCardPhonePeBL creditCardPhonePeBL = new CreditCardPhonePeBL();
+            //string templateFileName, baseUrl, restAPIRootUri, requestUri;
+            //baseUrl = "https://api-preprod.phonepe.com/";
+            //restAPIRootUri = "https://api-preprod.phonepe.com/apis/pg-sandbox/";
+            //requestUri = "pg/v1/pay";
+            //templateFileName = Server.MapPath("~/") + "PayPagePayLoad.json";
+            //creditCardPhonePeBL.ProcessPayLoad(templateFileName, baseUrl, restAPIRootUri, requestUri);
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult PhonePe(string paymentAmount)
+        {
+            CreditCardPhonePeBL creditCardPhonePeBL = new CreditCardPhonePeBL();
+            PhonePePayLoad phonePePayLoad = new PhonePePayLoad
+            {
+                BaseUrl = "https://api-preprod.phonepe.com/",
+                RestAPIRootUri = "https://api-preprod.phonepe.com/apis/pg-sandbox/",
+                RequestUri = "pg/v1/pay",
+                MerchantId = "PGTESTPAYUAT91",
+                MerchantTransactionId = Guid.NewGuid().ToString(),
+                MerchantUserId = "MUID123",
+                MerchantRedirectMode = "POST",
+                CreditCardAmount = "10000",
+                MerchantRedirectUrl = "https://webhook.site/c7c8645a-ddd4-4aa0-89f0-9997b4accbaf",
+                CustomerMobileNumber = "123456789",
+                PaymentInstrumentType = "PAY_PAGE",
+                SaltKey = "05992a0b-5254-4f37-86fb-e23bb79ea7e7",
+                SaltIndex = "1",
+                CheckStatusRestAPIRootUri = "",
+                CheckStatusRequestUri = ""
+            };
+            string templateFullFileName = Server.MapPath("~/") + "PhonePePayPagePayLoadTemplate.json";
+            PhonePeRestResponseObject responseObj = creditCardPhonePeBL.ProcessPhonePe(templateFullFileName, phonePePayLoad);
+            // Response.Redirect(responseObj.Data.InstrumentResponse.RedirectInfo.Url);
+            // return Json(responseObj);
+            return Json(responseObj);
+        }
+
+        [HttpPost]
+        public ActionResult PhonePeReturn(FormCollection formCollection)
+        {
+            // form["FirstName"] 
+            return View();
+        }
+    }
+}
---
+++ PhonePeWeb/CreditCardPhonePeBL_Old.cs
@@ -0,0 +1,141 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace PhonePeWeb
+{
+    public class CreditCardPhonePeBL_Old
+    {
+        public string Base64Encode(string plainText)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+        private string SHA256Hash(string randomString)
+        {
+            var crypt = new SHA256Managed();
+            string hash = String.Empty;
+            byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(randomString));
+            foreach (byte theByte in crypto)
+            {
+                hash += theByte.ToString("x2");
+            }
+            return hash;
+        }
+        public PhonePeRestResponseObject_Old ProcessPayLoad(string templateFileName, string baseUrl, string restAPIRootUri, string requestUri)
+        {
+            string payloadJson;
+            StreamReader streamReader = new StreamReader(templateFileName);
+            payloadJson = streamReader.ReadToEnd();
+            streamReader.Close();
+            string merchantId = "PGTESTPAYUAT91";
+            var merchantTransactionId = Guid.NewGuid().ToString();
+            payloadJson = payloadJson.Replace("<merchantTransactionId>", merchantTransactionId);
+            string base64EncodedPayload = Base64Encode(payloadJson);
+            var saltKey = "05992a0b-5254-4f37-86fb-e23bb79ea7e7";
+            var saltIndex = 1;
+            var stringToHash = base64EncodedPayload + "/pg/v1/pay" + saltKey;
+            var sha256 = SHA256Hash(stringToHash);
+            var finalXHeader = sha256 + "###" + saltIndex;
+            var options = new RestClientOptions(baseUrl)
+            {
+                MaxTimeout = -1,
+            };
+            var restClient = new RestClient(options);
+            //var request = new RestRequest("https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay", Method.Post);
+            var restRequest = new RestRequest(restAPIRootUri + requestUri, Method.Post);
+            restRequest.AddHeader("Content-Type", "application/json");
+            restRequest.AddHeader("X-VERIFY", finalXHeader);
+            var body = "{" + "\n" +
+                "\"request\":\"" +
+                base64EncodedPayload
+                + "\"\n" +
+                "}";
+            restRequest.AddStringBody(body, DataFormat.Json);
+            RestResponse restResponse = restClient.Execute(restRequest);
+            /*
+            1. Redirect to the info / url
+            2. Check Api Status - In a loop
+            3. Create a new page in our project
+            4. Pass this in the original payload
+            5. We should come back here
+            6. Check API status probably should be stopped
+            */
+            Console.WriteLine(restResponse.Content);
+            if (restResponse.IsSuccessful)
+            {
+                // Get the content of the response as a string
+                string responseContent = restResponse.Content;
+                PhonePeRestResponseObject_Old responseObject = JsonConvert.DeserializeObject<PhonePeRestResponseObject_Old>(responseContent);
+                return responseObject;
+            }
+            else
+            {
+                Console.WriteLine($"Request failed with status code: {restResponse.StatusCode}");
+                Console.WriteLine($"Error message: {restResponse.ErrorMessage}");
+                throw new Exception(restResponse.StatusCode + " " +restResponse.ErrorMessage);
+            }
+            //CheckApiStatus(restAPIRootUri, "pg/v1/status/", merchantId, merchantTransactionId, saltKey);
+        }
+        private void CheckApiStatus(string restAPIRootUri, string requestUri, string merchantId, string merchantTransactionId, string saltKey)
+        {
+            var saltIndex = 1;
+            var stringToHash = restAPIRootUri + requestUri + merchantId + "/" + merchantTransactionId + "/status" + saltKey;
+            var sha256 = SHA256Hash(stringToHash);
+            var finalXHeader = sha256 + "###" + saltIndex;
+
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(restAPIRootUri);
+
+            webRequest.Method = "GET";
+            webRequest.ContentType = "application/json";
+            webRequest.Headers.Add("x-verify", finalXHeader);
+
+            string responseData = string.Empty;
+
+            using (StreamReader responseReader = new StreamReader(webRequest.GetResponse().GetResponseStream()))
+            {
+                responseData = responseReader.ReadToEnd();
+                if (responseData.Length > 0)
+                {
+                    //PhonePeStatusResponseBody responseBody = JsonConvert.DeserializeObject<PhonePeStatusResponseBody>(responseData);
+                    Console.WriteLine(responseData);
+                    //Console.WriteLine(responseBody.message);
+                }
+            }
+        }
+    }
+    public class PhonePeInstrumentResponse
+    {
+        public string Type { get; set; }
+        public PhonePeRedirectInfo RedirectInfo { get; set; }
+    }
+
+    public class PhonePeRedirectInfo
+    {
+        public string Url { get; set; }
+        public string Method { get; set; }
+    }
+
+    public class PhonePeData
+    {
+        public string MerchantId { get; set; }
+        public string MerchantTransactionId { get; set; }
+        public PhonePeInstrumentResponse InstrumentResponse { get; set; }
+    }
+
+    public class PhonePeRestResponseObject_Old
+    {
+        public bool Success { get; set; }
+        public string Code { get; set; }
+        public string Message { get; set; }
+        public PhonePeData Data { get; set; }
+    }
+
+}
